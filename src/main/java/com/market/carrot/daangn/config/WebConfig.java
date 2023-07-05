f6bf588842a42ce3f1bd47d1474ce1ada155f4cb
purple-coco <eunsol1206@gmail.com>
2023-07-06 00:44:51 +0900

    필터 및 인터셉터 설정

---
+++ src/main/java/com/market/carrot/daangn/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.market.carrot.daangn.config;
+
+import com.market.carrot.daangn.filter.LoginCheckFilter;
+import com.market.carrot.daangn.interceptor.LoginCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/new", "/members/login", "/logout",
+                                    "/assets/images/*", "/css/*", "/js/*");
+    }
+
+//    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
--- src/main/java/com/market/carrot/daangn/controller/HomeController.java
+++ src/main/java/com/market/carrot/daangn/controller/HomeController.java
@@ -64,7 +64,8 @@ public class HomeController {
 
     @PostMapping("/members/login")
     public String LoginForm(@Valid @ModelAttribute("loginForm") MemberLoginForm form,
-                            BindingResult bindingResult, HttpServletRequest request) {
+                            BindingResult bindingResult, @RequestParam(defaultValue = "/") String redirectUrl,
+                            HttpServletRequest request) {
 
         if(bindingResult.hasErrors()) {
             return "members/loginMemberForm";
@@ -75,12 +76,13 @@ public class HomeController {
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호 확인이 필요합니다.");
             return "members/loginMemberForm";
-        } else {
-            HttpSession session = request.getSession();;
-            session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
-            return "redirect:/";
-
         }
+
+        HttpSession session = request.getSession();;
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectUrl;
+
 
     }
 
